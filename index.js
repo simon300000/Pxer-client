3107552ed78e9a5505547f0638dfa2bad46e22bc
simon3000 <simon3000@163.com>
2017-04-07 06:14:15 +0800

    Better

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 
-let mainWindow = null;
-
 app.on('ready', function() {
-    mainWindow = new BrowserWindow({
-    });
-
-    mainWindow.loadURL('http://www.pixiv.net/');
+    (new BrowserWindow({})).loadURL(`file://${__dirname}/index.html`)
 });
