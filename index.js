b2bbb5b48c77ff202e1b711964642e4e6ccdf00e
simon3000 <simon3000@163.com>
2017-04-07 06:34:21 +0800

    窗口开又关啊

--- index.js
+++ index.js
@@ -2,6 +2,37 @@ const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 
+let windowsNumber = 0
+
+process.on('createNewWindow', () => {
+    let window = new BrowserWindow({show:false})
+    window.loadURL(`file://${__dirname}/public/index.html`)
+    windowsNumber++
+    window.on('page-title-updated',()=>{
+      window.show()
+    })
+    window.on('closed', () => {
+        windowsNumber--
+    })
+})
+
 app.on('ready', function() {
-    (new BrowserWindow({})).loadURL(`file://${__dirname}/public/index.html`)
+    process.emit('createNewWindow')
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (windowsNumber === 0) {
+        process.emit('createNewWindow')
+    }
+})
+
+
+app.on('will-quit', function() {
+    //globalShortcut.unregisterAll();
 });
