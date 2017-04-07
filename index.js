65dfc341b672207d17bc5e793c3291d27ec8293f
simon3000 <simon3000@163.com>
2017-04-08 00:49:26 +0800

    记住窗口大小或全屏

--- index.js
+++ index.js
@@ -2,26 +2,37 @@ const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 
+const low = require('lowdb');
 const db = low('db.json')
+
+db.defaults({
+    config: {},
+    window: {
+        fullscreen: undefined,
+        size: [800, 600]
+    }
+}).write()
 
 let windowsNumber = 0
 
 process.on('createNewWindow', () => {
     let window = new BrowserWindow({
+        width: db.get('window.size[0]').value(),
+        height: db.get('window.size[1]').value(),
+        fullscreen: db.get('window.fullscreen').value(),
         show: false,
         frame: true
     })
-    window.loadURL(`file://${__dirname}/public/index.html`)
     windowsNumber++
-    window.on('page-title-updated', () => {
-        window.show()
-    })
-    window.on('closed', () => {
-        windowsNumber--
-    })
+    window.loadURL(`file://${__dirname}/public/index.html`)
+    window.on('page-title-updated', () => window.show())
+    window.on('closed', () => windowsNumber--)
+    window.on('resize', () => db.set('window.size', window.getSize()).write())
+    window.on('enter-full-screen', () => db.set('window.fullscreen', true).write())
+    window.on('leave-full-screen', () => db.set('window.fullscreen', undefined).write())
 })
 
-app.on('ready', function() {
+app.on('ready', () => {
     process.emit('createNewWindow')
 });
 
@@ -38,7 +49,7 @@ app.on('activate', () => {
 })
 
 
-app.on('will-quit', function() {
+app.on('will-quit', () => {
     //globalShortcut.unregisterAll();
 });
 
