1b27229645c4b1b3d76de3dd858d9e4a082b4fcf
simon3000 <simon3000@163.com>
2017-04-23 22:40:44 +0800

    暴力的下载方式呢

--- test.js
+++ test.js
@@ -1,9 +1,18 @@
 const fs = require('fs');
 const got = require('got');
 
-// Streams
-got.stream('url', {
-    headers: {
-        referer: 'https://www.pixiv.net/'
-    }
-}).pipe(fs.createWriteStream('name'));
+
+const down = (link) => {
+    got.stream(link, {
+        headers: {
+            referer: 'https://www.pixiv.net/'
+        }
+    }).pipe(fs.createWriteStream('down/' + link.match(/[^\/]+$/)));
+}
+
+
+let test = []//链接
+
+for (var i = 0; i < test.length; i++) {
+  down(test[i])
+}
